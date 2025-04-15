f3217589ee7184063624fc1ad1703ff3837b4317
Guillaume Giboury <ggiboury@k1r2p11.42mulhouse.fr>
2025-04-15 17:52:59 +0200

    Dictionary.hpp .cpp added. Files can be read, but not parsed

--- includes/Dictionary.hpp
+++ includes/Dictionary.hpp
@@ -6,13 +6,14 @@
 /*   By: ggiboury <ggiboury@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 16:44:26 by ggiboury          #+#    #+#             */
-/*   Updated: 2025/04/15 17:16:41 by ggiboury         ###   ########.fr       */
+/*   Updated: 2025/04/15 17:47:37 by ggiboury         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DICTIONARY_HPP
 # define DICTIONARY_HPP
 
+# include <fstream>
 # include <iostream>
 # include <list>
 
@@ -24,6 +25,7 @@ class Dictionary
 	private :
 		// For now, we use an list as it is the simplest container
 		std::list<Word> dict;
+		std::string _filename;
 
 	public :
 		Dictionary(std::string filename);
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,11 +1,27 @@
 #include "main.hpp"
 
+
+void test_word(void)
+{
+	std::cout << "Testing class Word" << std::endl;
+	Word test("sasdrgbtdgword");
+	std::cout << test.getWord() << std::endl;
+	std::cout << test.getColor(0) << std::endl;
+}
+
+void test_dictionary(void)
+{
+	std::cout << "Testing class Dictionary" << std::endl;
+	Dictionary t("words.txt");
+	t.parse();
+}
+
 int	main(void)
 {
 	std::cout << "TMP" << std::endl;
 
-	Word test("sasdrgbtdgword");
-	std::cout << test.getWord() << std::endl;
-	std::cout << test.getColor(0) << std::endl;
+	// test_word();
+	test_dictionary();
+
 	return (0);
 }
